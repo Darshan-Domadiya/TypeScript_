becaf7712bdceeeff90ddb3c3e87f5e40e7d0a2b
Darshan-Domadiya <darshandomadiya3525@gmail.com>
2025-02-17 18:01:13 -0500

    interface with functions

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 "use strict";
+// tuple example
 let arr = [12, "Darshan"];
+// void type function
 function hello() {
     console.log("Hello");
 }
+function getData(obj) {
+    obj.gender = "male";
+}
+getData({});
--- test.ts
+++ test.ts
@@ -1,7 +1,30 @@
 
-
+// tuple example
 let arr : [number , string] = [  12,"Darshan"];
 
+
+// void type function
 function hello():void{
     console.log("Hello");
 }
+
+// interface
+
+// user interface
+interface User{
+    name : string,
+    age : number,
+    password : string,
+    gender: string
+}
+
+function getData(obj : User){
+    obj.gender = "male"
+}
+
+
+
+getData({});
+
+
+
